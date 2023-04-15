2e1d39fea60007ce4cf58d372cb6beaae4838d89
Ewelina <ewelinazawol@gmail.com>
2023-04-15 13:40:52 +0100

    init

---
+++ sketch.js
@@ -0,0 +1,17 @@
+// import { VerletPhysics2D, VerletParticle2D, VerletSpring2D } from "toxiclibsjs";
+
+let physics;
+let particleA;
+
+function setup() {
+  createCanvas(640, 360);
+
+  //   physics = new VerletPhysics2D();
+}
+
+function draw() {
+  background(220);
+
+  fill();
+  circle(230, 100, 16);
+}
