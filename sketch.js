cb98bd145f7e0b02845caa7836dab02225d584e5
Ewelina <ewelinazawol@gmail.com>
2023-04-15 13:57:39 +0100

    toxiclibs

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ let particleA;
 function setup() {
   createCanvas(640, 360);
 
-  //   physics = new VerletPhysics2D();
+  physics = new toxi.physics2d.VerletPhysics2D();
 }
 
 function draw() {
@@ -14,4 +14,10 @@ function draw() {
 
   fill(0);
   circle(230, 100, 16);
+
+  var myVector = new toxi.geom.Vec2D(
+    window.innerWidth,
+    window.innerHeight
+  ).scaleSelf(0.5);
+  var myColor = toxi.color.TColor.newRGB(128 / 255, 64 / 255, 32 / 255);
 }
