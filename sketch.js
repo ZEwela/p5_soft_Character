a4687d046c38d2e9a01a6161d90088a5383ce9b4
Ewelina <ewelinazawol@gmail.com>
2023-04-16 13:01:21 +0100

    Spring Particle Classes

--- sketch.js
+++ sketch.js
@@ -4,34 +4,59 @@ const { GravityBehavior } = toxi.physics2d.behaviors;
 
 const { Vec2D, Rect } = toxi.geom;
 let physics;
-let particleA;
-let particleB;
+let particles = [];
+let springs = [];
 
 function setup() {
   createCanvas(640, 360);
 
   physics = new VerletPhysics2D();
 
-  let gravity = new GravityBehavior(new Vec2D(0, 1));
-  physics.addBehavior(gravity);
-
   let bounds = new Rect(0, 0, width, height);
   physics.setWorldBounds(bounds);
 
-  particleA = new VerletParticle2D(250, 100);
-  physics.addParticle(particleA);
-  particleB = new VerletParticle2D(300, 100);
-  physics.addParticle(particleB);
+  particles.push(new Particle(200, 100));
+  particles.push(new Particle(400, 100));
+  particles.push(new Particle(350, 200));
+  particles.push(new Particle(400, 300));
+  particles.push(new Particle(200, 300));
+  particles.push(new Particle(250, 200));
+
+  springs.push(new Spring(particles[0], particles[1], 0.1));
+  springs.push(new Spring(particles[1], particles[2], 0.1));
+  springs.push(new Spring(particles[2], particles[3], 0.1));
+  springs.push(new Spring(particles[3], particles[4], 0.1));
+  springs.push(new Spring(particles[4], particles[5], 0.1));
+  springs.push(new Spring(particles[5], particles[0], 0.1));
+  springs.push(new Spring(particles[5], particles[2], 0.1));
+  springs.push(new Spring(particles[0], particles[3], 0.1));
+  springs.push(new Spring(particles[1], particles[4], 0.1));
 }
 
 function draw() {
   background(220);
 
   physics.update();
+  fill(348);
+  stroke(0);
+  beginShape();
+  for (const particle of particles) {
+    vertex(particle.x, particle.y);
+  }
 
-  fill(0);
-  circle(particleA.x, particleA.y, 16);
+  endShape(CLOSE);
 
-  fill(0);
-  circle(particleB.x, particleB.y, 16);
+  if (mouseIsPressed) {
+    particles[0].lock();
+    particles[0].x = mouseX;
+    particles[0].y = mouseY;
+    particles[0].unlock();
+  }
+  // for (const particle of particles) {
+  //   particle.show();
+  // }
+
+  // for (const spring of springs) {
+  //   spring.show();
+  // }
 }
